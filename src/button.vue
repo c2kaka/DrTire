83ff5b5b9f86e49e48f4dcee5caf770fa3ea2fdc
“c2kaka <c2kaka@126.com>
2019-07-09 12:42:45 +0800

    完成按钮基本样式编写和parcel构建

---
+++ src/button.vue
@@ -0,0 +1,31 @@
+<template>
+    <button class="t-button">按钮</button>
+</template>
+
+<script>
+    export default {
+        name: "t-button"
+    }
+</script>
+
+<style lang="scss">
+    .t-button{
+        font-size: var(--font-size);
+        height: var(--button-height);
+        background: var(--button-bg);
+        padding: 0 1em;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        &:hover{
+            border-color: var(--border-color-hover);
+        }
+
+        &:active{
+            background: var(--button-active-bg);
+        }
+
+        &:focus{
+            outline: none;
+        }
+    }
+</style>
