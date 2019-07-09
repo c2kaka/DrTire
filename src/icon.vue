1bca3ad2c9a2d16d8bee3e894a88d05886f4df6b
“c2kaka <c2kaka@126.com>
2019-07-09 18:25:58 +0800

    完成对icon的组件化封装

---
+++ src/icon.vue
@@ -0,0 +1,18 @@
+<template>
+    <svg class="t-icon" aria-hidden="true">
+        <use :xlink:href=`#i-${icon}`></use>
+    </svg>
+</template>
+
+<script>
+    export default {
+        props:["icon"]
+    }
+</script>
+
+<style lang="scss">
+    .t-icon{
+        width: 1em;
+        height: 1em;
+    }
+</style>
