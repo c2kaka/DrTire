66096ee387086ce7b19973339fc5dc09604e5dd5
“c2kaka <c2kaka@126.com>
2019-07-16 10:07:53 +0800

    基本完成row和col的功能

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
-        <slot></slot>
+    <div class="col" :class="[`col-${span}`,offset && `offset-${offset}`]"
+         :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px' }">
+        <div style="border: 1px solid greenyellow;height: 100px">
+            <slot></slot>
+        </div>
     </div>
 </template>
 
@@ -10,6 +13,14 @@
         props: {
             span: {
                 type: [String, Number]
+            },
+            offset: {
+                type: [String, Number]
+            }
+        },
+        data() {
+            return {
+                gutter: 0
             }
         }
     }
@@ -17,15 +28,21 @@
 
 <style scoped lang="scss">
     .col {
-        height: 100px;
-        background: deepskyblue;
+        /*height: 100px;*/
+        /*background: deepskyblue;*/
         width: 50%;
-        border: 1px solid greenyellow;
+        /*border: 1px solid greenyellow;*/
 
         $classPrefix: col-;
         @for $n from 1 through 24 {
             &.#{$classPrefix}#{$n} {
                 width: ($n / 24) * 100%;
+            }
+        }
+        $classPrefix: offset-;
+        @for $n from 1 through 24 {
+            &.#{$classPrefix}#{$n} {
+                margin-left: ($n / 24) * 100%;
             }
         }
     }
