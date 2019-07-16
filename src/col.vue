960b3a9fdce31f4d18afc50926d36da68e2633b1
“c2kaka <c2kaka@126.com>
2019-07-16 09:00:01 +0800

    完成row和col的雏形

---
+++ src/col.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="col" :class="[`col-${span}`]">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "tireCol",
+        props: {
+            span: {
+                type: [String, Number]
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .col {
+        height: 100px;
+        background: deepskyblue;
+        width: 50%;
+        border: 1px solid greenyellow;
+
+        $classPrefix: col-;
+        @for $n from 1 through 24 {
+            &.#{$classPrefix}#{$n} {
+                width: ($n / 24) * 100%;
+            }
+        }
+    }
+</style>
---
+++ src/row.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="row">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "tireRow"
+    }
+</script>
+
+<style scoped lang="scss">
+    .row {
+        display: flex;
+    }
+</style>
