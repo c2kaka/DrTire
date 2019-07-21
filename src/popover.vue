713985a095a746cbe17b2446d6472a86ca7a87e8
fanoykaka <ouyangfanseu@gmail.com>
2019-07-21 22:41:13 +0800

    添加默认样式

--- src/popover.vue
+++ src/popover.vue
@@ -3,7 +3,7 @@
         <div class="content-wrapper" v-if="visible" ref="contentWrapper">
             <slot name="content"></slot>
         </div>
-        <span ref="triggerWrapper">
+        <span ref="triggerWrapper" style="display: inline-block">
           <slot></slot>
         </span>
     </div>
@@ -23,7 +23,7 @@
                 document.body.appendChild(contentWrapper);
                 const {top, left, height} = this.$refs.triggerWrapper.getBoundingClientRect();
                 contentWrapper.style.left = left + window.scrollX + 'px';
-                contentWrapper.style.top = top + window.scrollY - height + 'px';
+                contentWrapper.style.top = top + window.scrollY + 'px';
             },
             close() {
                 this.visible = false;
@@ -39,9 +39,12 @@
             onDocumentClick(e) {
                 if (this.$refs.popover && (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))) {
                     return undefined;
-                } else {
-                    this.close();
                 }
+                if (this.$refs.contentWrapper && (this.$refs.contentWrapper === e.target || this.$refs.contentWrapper.contains(e.target))) {
+                    return undefined;
+                }
+                this.close();
+
             },
             popoverClick(event) {
                 if (this.$refs.triggerWrapper.contains(event.target)) {
@@ -57,6 +60,8 @@
 </script>
 
 <style scoped lang="scss">
+    $border-color: #333;
+    $border-radius: 4px;
     .popover {
         display: inline-block;
         vertical-align: top;
@@ -66,7 +71,34 @@
     .content-wrapper {
         position: absolute;
         transform: translateY(-100%);
-        border: 1px solid #ddd;
-        box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);
+        border: 1px solid $border-color;
+        /*box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);*/
+        filter: drop-shadow(0 1px 3px rgba(0, 0, 0, 0.5));
+        background: white;
+        border-radius: $border-radius;
+        padding: .5em 1em;
+        margin-top: -10px;
+        word-break: break-all;
+        max-width: 20em;
+
+        &::before, &::after {
+            content: "";
+            display: block;
+            border: 10px solid transparent;
+            width: 0;
+            height: 0;
+            position: absolute;
+            left: 10px;
+        }
+
+        &::before {
+            border-top-color: black;
+            top: 100%;
+        }
+
+        &::after {
+            border-top-color: white;
+            top: calc(100% - 1px);
+        }
     }
 </style>
