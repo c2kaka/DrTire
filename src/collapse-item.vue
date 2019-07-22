27867a7cd1ffb928fe0ed18968e8d8956358840e
“c2kaka <c2kaka@126.com>
2019-07-22 11:53:36 +0800

    提高single配置参数

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="collapse-item">
-        <div class="title" @click="open=!open">
+        <div class="title" @click="toggle">
             {{title}}
         </div>
         <div class="content" v-if="open">
@@ -22,6 +22,27 @@
             return {
                 open: false
             }
+        },
+        inject: ['eventBus'],
+        methods: {
+            toggle() {
+                if (this.open) {
+                    this.open = false;
+                } else {
+                    this.open = true;
+                    this.eventBus && this.eventBus.$emit('update:selected', this);
+                }
+            },
+            close() {
+                this.open = false;
+            }
+        },
+        mounted() {
+            this.eventBus && this.eventBus.$on("update:selected", (vm) => {
+                if (vm !== this) {
+                    this.close();
+                }
+            })
         }
     }
 </script>
@@ -38,6 +59,7 @@
             min-height: 32px;
             display: flex;
             align-items: center;
+            cursor: pointer;
         }
 
         &:first-child {
